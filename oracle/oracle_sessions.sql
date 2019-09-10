ae9f268efc140e314ef1aed55d4642d0575e1e5b
Miquel Mariano <miquel.mariano0@gmail.com>
2019-09-10 14:17:45 +0200

    .

--- oracle/oracle_sessions.sql
+++ oracle/oracle_sessions.sql
@@ -2,25 +2,21 @@
 ---   Check Oracle Status
 
 ---NOTES 
----   File Name  : oracle_status.sql
+---   File Name  : oracle_sessions.sql
 ---   Author     : Miquel Mariano - @miquelMariano | https://miquelmariano.github.io
 ---   Version    : 1
 ---   Code       : https://github.com/miquelMariano/prtg-scripts/oracle
 
 ---USAGE
 ---   Put this script on C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\sql\oracle and use "Oracle SQL v2"
----   See: https://kb.paessler.com/en/topic/63259-how-can-i-monitor-strings-from-an-sql-database-and-show-a-sensor-status-depending-on-it  
 ---
 ---CHANGELOG
 ---   v1 10/09/2019   Script creation
 ---   
 
---- Original query: select status from v$instance;
-
-select  
-  CASE
-    WHEN status = 'OPEN' THEN 0
-    ELSE 100
-  END as status  
+select 
+	resource_name, current_utilization
 from 
-   v$instance;
+	v$resource_limit 
+where 
+	resource_name in (‘processes’,’sessions’);
--- oracle/oracle_tablespace.sql
+++ oracle/oracle_tablespace.sql
@@ -2,25 +2,24 @@
 ---   Check Oracle Status
 
 ---NOTES 
----   File Name  : oracle_status.sql
+---   File Name  : oracle_tablespace.sql
 ---   Author     : Miquel Mariano - @miquelMariano | https://miquelmariano.github.io
 ---   Version    : 1
 ---   Code       : https://github.com/miquelMariano/prtg-scripts/oracle
 
 ---USAGE
 ---   Put this script on C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\sql\oracle and use "Oracle SQL v2"
----   See: https://kb.paessler.com/en/topic/63259-how-can-i-monitor-strings-from-an-sql-database-and-show-a-sensor-status-depending-on-it  
 ---
 ---CHANGELOG
 ---   v1 10/09/2019   Script creation
 ---   
 
---- Original query: select status from v$instance;
 
-select  
-  CASE
-    WHEN status = 'OPEN' THEN 0
-    ELSE 100
-  END as status  
-from 
-   v$instance;
+SELECT 
+    tablespace_name,ROUND(sum(bytes)/1024/1024,0)
+FROM 
+    dba_free_space 
+WHERE 
+    tablespace_name in ('PSAPSR3','PSAPSR3740') 
+GROUP BY 
+    tablespace_name;
