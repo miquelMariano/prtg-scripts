fc69e0e06865e90615200f8328b1bd62ac7f1532
Miquel Mariano <miquel.mariano0@gmail.com>
2021-07-28 15:29:57 +0200

    Add more log detail as user logged by desktop and session details

--- vmware/vmware_horizon_pool_stats.ps1
+++ vmware/vmware_horizon_pool_stats.ps1
@@ -15,14 +15,15 @@
    .\vmware_horizon_pool_stats.ps1 -cs 192.168.241.63 -user mydomain\username -pool pool_name
    
 .CHANGELOG
-   v1 23/03/202021  Script creation
+   v1 23/03/2021	Script creation
+   v2 28/07/2021	Add more log detail as user logged by desktop and session details
   
 #>
 
 param(
-    [string]$cs = "vdi.mydomain.com",
-    [string]$user = "mydomain\miquel.mariano",
-	[string]$pool = "IC-WS2019-POOL"
+    [string]$cs = "vdi.mycorp.com",
+    [string]$user = "domain\miquel.mariano",
+	[string]$pool = "IC-WS2019-VSAN"
 )
 
 #--------------GLOBAL VARS----------------------
@@ -84,32 +85,56 @@ Connect-HVServer -Server $cs -credential $cred | out-null
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
 $log = "`r`n$now Connection Server $cs connected well!" + $log
 
-$desktops = Get-HVMachineSummary -PoolName $pool
+#$desktops = Get-HVMachineSummary -PoolName $pool
+$desktops = Get-HVMachineSummary -PoolName $pool | select @{n="DESKTOPNAME";e={$_.base.name}},@{n="STATE";e={$_.base.basicstate}},@{n="ESXi";e={$_.managedmachinenamesdata.hostname}}
 [int]$total = $desktops.count
 
 ForEach ($desktop in $desktops){ 
+  $sessiondata = (get-hvlocalsession).namesdata | where machineorrdsservername -eq $desktop.desktopname
   $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
-  $log = "`r`n$now Desktop "+ $desktop.base.name + " is in a "+ $desktop.base.basicstate + " state." + $log
-    }
+  $log = "`r`n$now Desktop "+ $desktop.desktopname + " is in a "+ $desktop.state + " state. Used by" + $sessiondata.username + " | Client type: " + $sessiondata.clienttype + " | Client IP: " + $sessiondata.clientaddress + " | Client name: " + $sessiondata.clientname + " | Client version: " + $sessiondata.clientversion + " | Client source: " + $sessiondata.securitygatewaylocation + "." + $log
+#  if ( $desktop.base.basicstate -eq "ALREADY_USED" ) {
+#     Remove-HVMachine -machinenames $desktop.base.name -deletefromdisk:$true -confirm:$false
+#     $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+#     $log = "`r`n$now IMPORTANT Deleting "+ $desktop.base.name + " because is in a "+ $desktop.base.basicstate + " state." + $log
+#  }
+}
 
-$Available = ($desktops | where {$_.base.basicstate -eq "AVAILABLE"}).count;
-$Connected = ($desktops | where {$_.base.basicstate -eq "CONNECTED"}).count;
-$Disconnected = ($desktops | where {$_.base.basicstate -eq "DISCONNECTED"}).count;
-$Maintenance = ($desktops | where {$_.base.basicstate -eq "MAINTENANCE"}).count;
-$Provisioning = ($desktops | where {$_.base.basicstate -eq "PROVISIONING"}).count;
-$Customizing = ($desktops | where {$_.base.basicstate -eq "CUSTOMIZING"}).count;
-$Already_Used = ($desktops | where {$_.base.basicstate -eq "ALREADY_USED"}).count;
-$Agent_Unreachable = ($desktops | where {$_.base.basicstate -eq "AGENT_UNREACHABLE"}).count;
-$Error = ($desktops | where {$_.base.basicstate -eq "ERROR"}).count;
-$Deleting = ($desktops | where {$_.base.basicstate -eq "DELETING"}).count;
-$Provisioning_Error = ($desktops | where {$_.base.basicstate -eq "PROVISIONING_ERROR"}).count;
+##SESSION DATA VARIABLES
+$sessiondataglobal = (get-hvlocalsession).namesdata | where desktoppoolcn -eq $pool
+$externalconnection = ($sessiondataglobal | where {$_.securitygatewaylocation -eq "External"}).count
+$internalconnection = ($sessiondataglobal | where {$_.securitygatewaylocation -eq "Internal"}).count
+$unknownconnection = ($sessiondataglobal | where {$_.securitygatewaylocation -eq "Unknown"}).count
+$windowsclient = ($sessiondataglobal | where {$_.clienttype -eq "Windows"}).count
+$linuxclient = ($sessiondataglobal | where {$_.clienttype -eq "Linux"}).count
+$macclient = ($sessiondataglobal | where {$_.clienttype -eq "Mac"}).count
+$otherclient = ($sessiondataglobal | where {$_.clienttype -eq "Other"}).count
+$clientversion4100 = ($sessiondataglobal | where {$_.clientversion -eq "4.10.0"}).count
+$clientversion490 = ($sessiondataglobal | where {$_.clientversion -eq "4.9.0"}).count
+$clientversion530 = ($sessiondataglobal | where {$_.clientversion -eq "5.3.0"}).count
+$clientversion544= ($sessiondataglobal | where {$_.clientversion -eq "5.4.4"}).count
+$clientversion800 = ($sessiondataglobal | where {$_.clientversion -eq "8.0.0"}).count
+$clientversion830 = ($sessiondataglobal | where {$_.clientversion -eq "8.3.0"}).count
+
+#DESKTOP STATUS VARIABLES
+$Available = ($desktops | where {$_.state -eq "AVAILABLE"}).count;
+$Connected = ($desktops | where {$_.state -eq "CONNECTED"}).count;
+$Disconnected = ($desktops | where {$_.state -eq "DISCONNECTED"}).count;
+$Maintenance = ($desktops | where {$_.state -eq "MAINTENANCE"}).count;
+$Provisioning = ($desktops | where {$_.state -eq "PROVISIONING"}).count;
+$Customizing = ($desktops | where {$_.state -eq "CUSTOMIZING"}).count;
+$Already_Used = ($desktops | where {$_.state -eq "ALREADY_USED"}).count;
+$Agent_Unreachable = ($desktops | where {$_.state -eq "AGENT_UNREACHABLE"}).count;
+$Error = ($desktops | where {$_.state -eq "ERROR"}).count;
+$Deleting = ($desktops | where {$_.state -eq "DELETING"}).count;
+$Provisioning_Error = ($desktops | where {$_.base.state -eq "PROVISIONING_ERROR"}).count;
 
 $all_states = $available + $connected + $disconnected + $maintenance + $provisioning + $customizing + $already_used + $agent_unreachable + $error + $deleting + $provisioning_error
 
 #write-host Total VMs ------------------ $desktops.count
 #write-host Disponibles ---------------- $available
-#write-host Conectadas ----------------- $connected
-#write-host Desconectadas -------------- $disconnected
+write-host Conectadas ----------------- $connected
+write-host Desconectadas -------------- $disconnected
 #write-host Mantenimiento -------------- $maintenance
 #write-host Provisionando -------------- $provisioning
 #write-host Customizando --------------- $customizing
@@ -190,10 +215,101 @@ $XMLOutput += "<channel>Suma de todos los estados</channel> `n"
 $XMLOutput += "<value>" + $all_states + "</value>`n"
 $XMLOutput += "</result>`n"
 
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes externos</channel> `n"
+$XMLOutput += "<value>" + $externalconnection + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes internos</channel> `n"
+$XMLOutput += "<value>" + $internalconnection + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes origen desconocido</channel> `n"
+$XMLOutput += "<value>" + $unknownconnection + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes Windows</channel> `n"
+$XMLOutput += "<value>" + $windowsclient + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes Linux</channel> `n"
+$XMLOutput += "<value>" + $linuxclient + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes Mac</channel> `n"
+$XMLOutput += "<value>" + $macclient + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes SO desconocido</channel> `n"
+$XMLOutput += "<value>" + $otherclient + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 4.10.0</channel> `n"
+$XMLOutput += "<value>" + $clientversion4100 + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 4.9.0</channel> `n"
+$XMLOutput += "<value>" + $clientversion490 + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 5.3.0</channel> `n"
+$XMLOutput += "<value>" + $clientversion530 + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 5.4.4</channel> `n"
+$XMLOutput += "<value>" + $clientversion544 + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 8.0.0</channel> `n"
+$XMLOutput += "<value>" + $clientversion800 + "</value>`n"
+$XMLOutput += "</result>`n"
+
+$XMLOutput += "<result>`n"
+$XMLOutput += "<channel>Clientes version 8.3.0</channel> `n"
+$XMLOutput += "<value>" + $clientversion830 + "</value>`n"
+$XMLOutput += "</result>`n"
+
 $XMLOutput += "</prtg>"
 
 write-host $XMLOutput
 
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 8.3.0 -------------- $clientversion830" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 8.0.0 -------------- $clientversion800" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 5.4.4 -------------- $clientversion544" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 5.3.0 -------------- $clientversion530" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 4.10.0 ------------- $clientversion4100" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Clients 4.9.0 -------------- $clientversion490" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Other client --------------- $otherclient" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Mac client ----------------- $macclient" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Linux client --------------- $linuxclient" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Windows client ------------- $windowsclient" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Unknown source conection --- $unknownconnection" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now Internal conection --------- $internalconnection" + $log
+$now = Get-Date -format "dd-MM-yy HH:mm:ss | "
+$log = "`r`n$now External conection --------- $externalconnection" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
 $log = "`r`n$now Provisioning error --------- $provisioning_error" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
@@ -211,7 +327,7 @@ $log = "`r`n$now Provisioning --------------- $provisioning" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
 $log = "`r`n$now Maintenance ---------------- $maintenance" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
-$log = "`r`n$now Desconnected --------------- $disconnected" + $log
+$log = "`r`n$now Disconnected --------------- $disconnected" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
 $log = "`r`n$now Connected ------------------ $connected" + $log
 $now = Get-Date -format "dd-MM-yy HH:mm:ss | "
