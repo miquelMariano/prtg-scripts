cae97135726705219de390dd1e93c360864617de
Miquel Mariano <miquel.mariano0@gmail.com>
2023-06-01 15:16:16 +0200

    Update vmware_check_esxi_ntpd.sh

--- vmware/vmware_check_esxi_ntpd.sh
+++ vmware/vmware_check_esxi_ntpd.sh
@@ -11,25 +11,19 @@
 #USAGE
 #   - Put this script on /var/prtg/scripts and use "SSH Script"
 #	- It's necessary add ssh credentials on device
-#   - Command return follow results:
-#   [root@esxi:/var/prtg/scripts] esxcli vsan debug resync summary get
-#       Total Number Of Resyncing Objects: 0
-#       Total Bytes Left To Resync: 0
-#       Total GB Left To Resync: 0.00
-
+#   
 #   
 #CHANGELOG
 #   v1 01/06/2023   Script creation
 
-
 result=$(/etc/init.d/ntpd status)
 
-if [ $result == "ntp is running" ]; then
+if [ "$result" = "ntpd is running" ]; then
 	echo "0:0:$result"
 fi
 
-if [ $result == "ntp is not running" ]; then
-	echo "2:0:$result"
+if [ "$result" = "ntpd is not running" ]; then
+	echo "2:1:$result"
 fi
 
 
